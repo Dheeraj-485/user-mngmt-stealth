16bda4aa81d22cf893b6799f20871c87b9075c18
Dheeraj-485 <118504120+Dheeraj-485@users.noreply.github.com>
2025-03-31 19:43:34 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,31 +1,4 @@
-// const jwt = require("jsonwebtoken");
-// const { decode } = require("punycode");
 
-// require("dotenv").config();
-
-// exports.isAuth = async (req, res, next) => {
-//   try {
-//     const token = req.header("Authorization")?.split(" ")[1];
-
-//     if (!token) {
-//       return res.status(401).json({ message: "Token not found or expired" });
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     console.log("decoded", decoded);
-
-//     if (!decoded) {
-//       return res.status(402).json({ message: "UnAuthorized user" });
-//     }
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     console.error("Error in authorization", error.message);
-//     return res
-//       .status(500)
-//       .json({ message: "Error in authorization", error: error.message });
-//   }
-// };
 
 const jwt = require("jsonwebtoken");
 
